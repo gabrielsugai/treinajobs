250410eb07de5b2ebaef99df81c0230e20483e19
Gabriel Sugai <gabrielsugai@gmail.com>
2020-05-24 13:48:45 -0300

    Comment canot be blank

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user_profile
   belongs_to :headhunter
+
+  validates :text, presence: {message: 'Comentario não pode ficar em branco'}
 end
--- spec/features/headhunter/headhunter_make_comment_in_user_profile_spec.rb
+++ spec/features/headhunter/headhunter_make_comment_in_user_profile_spec.rb
@@ -17,4 +17,20 @@ feature 'Headhunter can comment on user profile' do
     expect(page).to have_content('Gostei das suas habilidades')
     expect(current_path).to eq(user_profile_path(user_profile))
   end
+
+  scenario 'and comment canot be blank' do
+    headhunter = create(:headhunter)
+    user_profile = create(:user_profile)
+    job = create(:job, headhunter: headhunter)
+    opportunity = create(:opportunity, job: job, user_profile: user_profile)
+
+    login_as headhunter, scope: :headhunter
+
+    visit user_profile_path(user_profile)
+
+    fill_in 'Digite aqui:', with: ''
+    click_on 'Comentar'
+
+    expect(page).to have_content('Comentario não pode ficar em branco')
+  end
 end  
